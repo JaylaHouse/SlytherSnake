f6b4e49cf10899107bbd32d4d62a7d96eaf0d230
Jayla House <jdh390@txstate.edy>
2022-02-21 14:03:24 -0600

    Snake Follows Mouse and can grow more parts

---
+++ SlytherSnake/Assets/Scripts/SnakeController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour
+{
+    //Settings
+    public float BodySpeed = 0.1f;
+    public int Gap = 10;
+
+    // References
+    public GameObject BodyPrefab;
+
+    // Lists
+    private List<GameObject> BodyParts = new List<GameObject>();
+    private List<Vector3> PositionsHistory = new List<Vector3>();
+
+    void Start()
+    {
+        //making snake parts spawn at different times 
+        Invoke("GrowSnake", 1);
+        Invoke("GrowSnake", 2);
+        Invoke("GrowSnake", 3);
+        Invoke("GrowSnake", 4);
+        Invoke("GrowSnake", 5);
+        Invoke("GrowSnake", 6);
+        
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // Store position history
+        PositionsHistory.Insert(0, transform.position);
+
+        // Move body parts
+        int index = 0;
+        foreach (var body in BodyParts)
+        {
+            Vector3 point = PositionsHistory[Mathf.Clamp(index * Gap, 0, PositionsHistory.Count - 1)];
+
+            // Move body towards the point along the snakes path
+            Vector3 moveDirection = point - body.transform.position;
+            //body.transform.position += moveDirection * BodySpeed * Time.deltaTime;        //original code but wasnt working for me
+            //beginning of change 
+            body.transform.Translate(moveDirection * BodySpeed);
+            //end of change 
+
+            // Rotate body towards the point along the snakes path
+            body.transform.LookAt(point);
+
+            index++;
+        }
+
+        
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 mousePos = hit.point;
+
+            Vector3 dir = hit.point - transform.position;
+            dir.y = 0;
+
+            float speed = 0.1f;
+            if (dir.magnitude > speed)
+            {
+                dir.Normalize();
+                transform.Translate(dir * speed);
+            }
+
+            Vector3 spherePos = transform.position;
+            spherePos.y = 0;
+            //characterMarkerMat.SetVector("Vector3_337113A", spherePos);
+        }
+    }
+    private void GrowSnake()
+    {
+        // Instantiate body instance and
+        // add it to the list
+        GameObject body = Instantiate(BodyPrefab);
+        BodyParts.Add(body);
+    }
+}
