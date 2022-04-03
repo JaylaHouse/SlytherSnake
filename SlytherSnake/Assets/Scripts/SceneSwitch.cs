633b16683f0d4e8542688c0e9da9b7d5b4c18ed6
leonardo <l_b384@txstate.edu>
2022-04-03 00:22:13 -0500

    SNAK-49 Level Transition Added

---
+++ SlytherSnake/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SceneSwitch : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other){
+        
+        Application.LoadLevel(1);
+        Debug.Log("Collided");
+
+    }
+
+}
